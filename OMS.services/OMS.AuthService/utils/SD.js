d2ad7c31886edf0d37458813cb9582886f901c34
Ariyo Jones <120025384+cyber-jones@users.noreply.github.com>
2025-06-09 07:08:37 +0100

    Production (Fourth Commit)

--- OMS.services/OMS.AuthService/utils/SD.js
+++ OMS.services/OMS.AuthService/utils/SD.js
@@ -4,7 +4,7 @@ export const client_production_uri = process.env.CLIENT_PRODUCTION_URI;
 
 export const ROLES = [ "admin", "doctor", "staff", "patient" ];
 
-export const ALLOWEDORIGINS_Dev = ["http://localhost:7001", "http://localhost:7002", "http://localhost:7003", "http://localhost:7004", "http://localhost:5173", "https://oms-ochre.vercel.app"];
+export const ALLOWEDORIGINS = ["http://localhost:7001", "http://localhost:7002", "http://localhost:7003", "http://localhost:7004", "http://localhost:5173", "https://oms-ochre.vercel.app"];
 
 export const ALLOWEDMETHODS = ['GET', 'POST', 'PUT', 'DELETE'];
 

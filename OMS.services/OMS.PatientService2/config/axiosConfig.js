d449341fefd1581679d3df0bdc2dd91a66efaa48
Ariyo Jones <120025384+cyber-jones@users.noreply.github.com>
2025-07-04 20:18:00 +0100

    Some random commit

--- OMS.services/OMS.PatientService2/config/axiosConfig.js
+++ OMS.services/OMS.PatientService2/config/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const axiosPrivate = (token) => {
     const axiosPrivate = axios.create({
-        baseURL: "http://localhost:7005/api/user",
+        // baseURL: "http://localhost:7005/api/user",
+        baseURL: "https://oms-auth-api.onrender.com/api/user",
         headers: {
             "Content-Type": "application/json",
             "Authorization": "Bearer "+ token
